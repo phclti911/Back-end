9166df15536e98440d062997402f7f8201d40b7c
phclti911 <126952576+phclti911@users.noreply.github.com>
2024-03-15 09:49:13 -0300

    Add files via upload

---
+++ The Greatest (Com Console) .php
@@ -0,0 +1,25 @@
+<?php
+$a= readline("digite o valor de do numero 1 ");
+$b= readline("digite o valor de do numero 2 ");
+$c= readline("digite o valor de do numero 3 ");
+$maiorAB=($a+$b+abs($a-$b))/2;
+$maiorC=($maiorAB+$c+abs($maiorAB-$c))/2;
+echo "$maiorC é o maior";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
